7f52d3a37609eb2d8c450442b6dcfaeaaaa3d63b
kevin <kevinrichard31@gmail.com>
2024-11-29 22:13:54 +0100

    describe fix

--- src/pages/Describe.tsx
+++ src/pages/Describe.tsx
@@ -1,10 +1,25 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonIcon, useIonRouter } from '@ionic/react';
+import { 
+  IonContent, 
+  IonHeader, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar, 
+  IonButton, 
+  IonIcon 
+} from '@ionic/react';
 import { useHistory } from 'react-router-dom'; 
 import './Describe.css';
 import { useEmotion } from '../contexts/EmotionContext';
 import { useEffect, useRef, useState } from 'react';
 import { createUser, saveEmotion } from '../utils/api';
-import { arrowBack } from 'ionicons/icons'; // Utilisation de l'icône Ion
+import { arrowBack } from 'ionicons/icons';
+
+interface EmotionDetails {
+  latitude: number | null;
+  longitude: number | null;
+  emotionName: string | null;
+  description: string;
+}
 
 const Describe: React.FC = () => {
   const { emotion, image, background, latitude, longitude } = useEmotion();
@@ -12,61 +27,61 @@ const Describe: React.FC = () => {
   const [isSaving, setIsSaving] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const history = useHistory();
-  
 
   useEffect(() => {
-    // Sélectionner ion-content
     const ionContentElement = document.querySelector('ion-content');
-    
+
     if (ionContentElement && background) {
-      // Utiliser setProperty pour appliquer la couleur d'arrière-plan dynamiquement
       ionContentElement.style.setProperty('--ion-background-color', background);
     }
-  
+
     if (inputRef.current) {
       inputRef.current.focus();
     }
-  
+
     return () => {
       if (ionContentElement) {
-        // Réinitialiser la couleur de fond lors du démontage du composant
         ionContentElement.style.removeProperty('--ion-background-color');
       }
     };
   }, [background]);
-  
-  
-  
+
+  const asyncLocalStorage = {
+    getItem: async (key: string): Promise<string | null> => {
+      return Promise.resolve(localStorage.getItem(key));
+    },
+    setItem: async (key: string, value: string): Promise<void> => {
+      return Promise.resolve(localStorage.setItem(key, value));
+    },
+  };
 
   const handleSave = async () => {
     let userId = await asyncLocalStorage.getItem('userId');
     let userPassword = await asyncLocalStorage.getItem('password');
-  
+
     if (!userId) {
-      userId = await createUser();
-      if (!userId) {
+      const user = await createUser();
+      if (!user || !user.id) {
         setError('Failed to create user');
         return;
       }
-      console.log(userId + 'created')
-      console.log(userId.id + 'created')
-      await asyncLocalStorage.setItem('userId', userId.id);
-      userId = userId.id
+      console.log(`${user.id} created`);
+      await asyncLocalStorage.setItem('userId', user.id);
+      userId = user.id;
     }
- 
-  
+
     setIsSaving(true);
     setError(null);
-  
-    const emotionDetails = {
-      latitude,
-      longitude,
-      emotionName: emotion,
+
+    const emotionDetails: EmotionDetails = {
+      latitude: latitude || 0, // Assurez-vous d'avoir des valeurs par défaut si `latitude` ou `longitude` est null
+      longitude: longitude || 0,
+      emotionName: emotion || '',
       description: inputRef.current?.value || '',
     };
-  
-    const result = await saveEmotion(userId,userPassword, emotionDetails);
-  
+
+    const result = await saveEmotion(userId, userPassword || '', emotionDetails);
+
     if (result) {
       console.log('Emotion saved successfully:', result);
       const emotionDate = new Date().toISOString().split('T')[0];
@@ -74,34 +89,23 @@ const Describe: React.FC = () => {
     } else {
       setError('Failed to save emotion');
     }
-  
+
     setIsSaving(false);
   };
-  
 
-  const asyncLocalStorage = {
-    getItem: async (key) => {
-      return Promise.resolve(localStorage.getItem(key));
-    },
-    setItem: async (key, value) => {
-      return Promise.resolve(localStorage.setItem(key, value));
-    },
-  };
-  
   return (
     <IonPage className="describe">
-
       <IonContent>
         <img src={image} className="emoji-size" alt="Emotion" />
-        <div className='container-input'>
-          <div className='describe-title'>Décris ce qui se passe :</div>
+        <div className="container-input">
+          <div className="describe-title">Décris ce qui se passe :</div>
           <textarea 
             ref={inputRef} 
-            className='inputTextDescribe' 
-            placeholder='Décris ton émotion' 
+            className="inputTextDescribe" 
+            placeholder="Décris ton émotion" 
           />
           <IonButton 
-            className='button-next' 
+            className="button-next" 
             onClick={handleSave} 
             disabled={isSaving}
             expand="full"
