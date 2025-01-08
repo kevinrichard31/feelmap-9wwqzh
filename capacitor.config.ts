a979727cfeae5457f6d1f8260d546348300fbbaf
kevinrichard31 <kevinrichard31@gmail.com>
2025-01-09 00:44:55 +0100

    update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.feelmap.app',
   appName: 'feelmap',
   webDir: 'dist'
 };
